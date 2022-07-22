d562248a3bf8688865f67cb5c20fff9dfbcd5f36
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-22 09:50:42 +0100

    connect

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+ echo'hello';
+ $DB_NAME='moneyvar_users';
+ $DB_USER='moneyvar_user';
+ $DB_PASSWORD='t~m~{%?KmrRa';
+ $DB_HOST='localhost:3306';
+ $conn= mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME");
+ 
+?>
